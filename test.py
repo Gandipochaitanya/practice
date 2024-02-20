c97b91f2ee5d55436c76639f3cd7d64d54343a45
Gandipochaitanya <gandipochaitanya@gmail.com>
2024-02-20 12:47:06 +0530

    first commit

---
+++ test.py
@@ -0,0 +1,120 @@
+print('chaitanya')
+print('data types')
+print('10+20')
+a=2
+b=4
+print(a+b)
+name="chaitanya"
+age=22
+rollno=511
+mobileNumber=9876543210
+print(mobileNumber)
+aadhar=619732459794
+print(aadhar)
+motherName="bhavani"
+print(motherName)
+email="gandipochaitanya@gmail.com"
+print(email)
+address="3-152/4,sanjeevi nagar,rajamundrty"
+print(address)
+grade="1st class"
+print(grade)
+percentage=9.0
+print(percentage)
+print(name,age)
+print(a-b)
+print(a/b)
+print("my name is",name,"and my age is",age,"and my rollno is",rollno)
+print('2'+'2')
+print(2+2)
+print("my name is",name,"and my age is",age,"and my rollno is",rollno,\
+      "and my mobileNumber is",mobileNumber,"and my aadhar is",aadhar,\
+      "and my motherName is",motherName,"and my email is",email,\
+      "and my address is",address,"and my grade is",grade,\
+      "and my percentage is",percentage)
+trainNo=32478
+print(trainNo)
+trainName="SVMB SPL"
+print(trainName)
+destination="BZA to BENGULURU"
+print(destination)
+PNR=6231793566
+print(PNR)
+name="chaitanya"
+print(name)
+age=22
+print(age)
+gender="female"
+print(gender)
+email="gandipochaitanya@gmail.com"
+print(email)
+mobileNo=9876543210
+print(mobileNo)
+address="3-152/4,sanjeevi nagar,rajamundry"
+print(address)
+bookingStatus="CNF/B3/26/MB"
+print(bookingStatus)
+ticketFare="800."
+print(ticketFare)
+print (trainName,"train with train number",trainNo," is scheduled from",destination,\
+       ".The passenger check the running status with the use of pnr no is",PNR,\
+       "In that train passenger name is ",name,",age",age,",identified as ",gender,\
+       "her mobile number and email is",mobileNo,",",email,".Her native place is",address,\
+       " and their booking status is ",bookingStatus,",the ticket fare is",ticketFare)
+
+
+
+      
+companyName="teachNook"
+print(companyName)
+companyAddress="167,9th Main Rd, near to Empire Restaurant, Sector 6, HSR Layout, Bengaluru, Karnataka 560102"
+print(companyAddress)
+employeeName="ramya"
+print(employeeName)
+age=26
+print(age)
+employeeId=234
+print(employeeId)
+position="devloper"
+print(position)
+salary=50000
+print(salary)
+mobileNo=7396452585
+print(mobileNo)
+emailId="ramkotti2143@gmail.com"
+print(emailId)
+address="3-48,tilak street,kakinada,kakinada district,Andhra Pradesh."
+print(address)
+nationality="INDIAN"
+print("An",companyName,"is a software company,which is located at ",companyAddress,\
+      ".IN this company",employeeName,"is an employee she has",age,"she is a",position,\
+      "with",salary,"package and her id is",employeeId,"she is an",nationality,"and her",\
+      "mobile number and email id is",mobileNo,",",emailId)
+HotelName="Shelton"
+print(HotelName)
+HotelAddress="87-1-19/B1, Morampudi Rd, beside Ramakrishna Theatre, Ayyappa Nagar,",\
+"Rajamahendravaram, Andhra Pradesh 533103"
+print(HotelAddress)
+CustomerName="Riya"
+print(CustomerName)
+FatherName="Deva"
+print(FatherName)
+mobileNo=9632584710
+print(mobileNo)
+emailId="riya143@gmail.com"
+print(emailId)
+address="3-58,Banugudi Junction,kakinada,kakinada District,Pradesh ."
+print(address)
+aadharNo=898654324576
+print(aadharNo)
+monthOf="FEB,2022"
+print(monthOf)
+cost=20000
+print(cost)
+maintanceCharge=5000
+print(maintanceCharge)
+adavance=5000
+print(adavance)
+
+
+
